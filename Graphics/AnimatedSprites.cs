35e83c6bb1070d37e59ff54e24eef8c2047dc798
RjanDoodles <rjsanchez023@gmail.com>
2025-11-11 12:42:22 -0600

    Release One

---
+++ Graphics/AnimatedSprites.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Game_Library.Graphics
+{
+    public class AnimatedSprites : Sprite
+    {
+        public int _currentFrame;
+        private TimeSpan _elapsed;
+        public Animation _animation;
+        TimeSpan estimatedDelay;
+        public bool stop;
+
+        /// <summary>
+        /// Gets or Sets the animation for this animated sprite.
+        /// </summary>
+        public Animation Animation
+        {
+            get => _animation;
+            set
+            {
+                _animation = value;
+                Region = _animation.Frames[0];
+            }
+        }
+
+        /// <summary>
+        /// Creates a new animated sprite.
+        /// </summary>
+        public AnimatedSprites() { }
+
+        /// <summary>
+        /// Creates a new animated sprite with the specified frames and delay.
+        /// </summary>
+        /// <param name="animation">The animation for this animated sprite.</param>
+        public AnimatedSprites(Animation animation)
+        {
+            Animation = animation;
+        }
+
+        public void swapAnimations(Animation newAnimation)
+        {
+            _currentFrame = 0;
+            _animation = newAnimation;
+            _animation.finished = false;
+            estimatedDelay = TimeSpan.FromMilliseconds(_animation.Delay.TotalMilliseconds * _animation.FrameCounts[_currentFrame]);
+            _elapsed.Equals(0);
+        }
+        /// <summary>
+        /// Updates this animated sprite.
+        /// </summary>
+        /// <param name="gameTime">A snapshot of the game timing values provided by the framework.</param>
+        public void Update(GameTime gameTime)
+        {
+            _elapsed += gameTime.ElapsedGameTime;
+            if (_elapsed >= estimatedDelay && !stop)
+            {
+                _elapsed -= estimatedDelay;
+                if (_animation.loop)
+                {
+                    if (_currentFrame >= _animation.Frames.Count - 1)
+                    {
+                        _currentFrame = 0;
+                    }
+                    else
+                    {
+                        _currentFrame++;
+                        estimatedDelay = TimeSpan.FromMilliseconds(_animation.Delay.TotalMilliseconds * _animation.FrameCounts[_currentFrame]);
+                    }
+                }
+                else
+                {
+                    if (!_animation.finished)
+                    {
+                        if (_currentFrame >= _animation.Frames.Count - 1)
+                        {
+                            _animation.finished = true;
+                        }
+                        else
+                        {
+                            _currentFrame++;
+                            estimatedDelay = TimeSpan.FromMilliseconds(_animation.Delay.TotalMilliseconds * _animation.FrameCounts[_currentFrame]);
+                        }
+                    }
+                }
+
+                Region = _animation.Frames[_currentFrame];
+            }
+        }
+
+    }
+}
---
+++ Graphics/Animation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game_Library.Graphics
+{
+    public class Animation
+    {
+        public string Name { get; set; }
+        /// <summary>
+        /// The texture regions that make up the frames of this animation.  The order of the regions within the collection
+        /// are the order that the frames should be displayed in.
+        /// </summary>
+        public List<TextureRegion> Frames { get; set; }
+
+        /// <summary>
+        /// The amount of time to delay between each frame before moving to the next frame for this animation.
+        /// </summary>
+        /// <remarks>
+        /// Default is a 30 FPS Animation (500 Milliseconds)
+        /// </remarks>
+        public TimeSpan Delay { get; set; } = TimeSpan.FromMilliseconds(500);
+        /// <summary>
+        /// Will the animation loop once it plays all it's frames
+        /// </summary>
+        /// <remarks>
+        /// Default is set to true
+        /// </remarks>
+        public bool loop { get; set; } = true;
+        /// <summary>
+        /// If the animation is finshed
+        /// </summary>
+        public bool finished { get; set; } = false;
+        /// <summary>
+        /// The frames the animation is animated on
+        /// </summary>
+        public List<int> FrameCounts { get; set; }
+        /// <summary>
+        /// Creates a new animation.
+        /// </summary>
+        public Animation()
+        {
+            Frames = new List<TextureRegion>();
+            Delay = TimeSpan.FromMilliseconds(100);
+        }
+
+        /// <summary>
+        /// Creates a new animation with the specified frames and delay.
+        /// </summary>
+        /// <param name="frames">An ordered collection of the frames for this animation.</param>
+        /// <param name="delay">The amount of time to delay between each frame of this animation.</param>
+        /// <param name="looping">Will the animation reset once it hits the final frame.</param>
+        /// <param name="done">If you set looping to false, and need an event to happen once it finishes, the animation file will read this.</param>
+        /// <param name="frameCounts">The amount of times a frame is stayed on, 1 is the default.</param>
+        public Animation(string name, List<TextureRegion> frames, TimeSpan delay, bool looping, bool done, List<int> frameCounts)
+        {
+            Name = name;
+            Frames = frames;
+            Delay = delay;
+            loop = looping;
+            finished = done;
+            FrameCounts = frameCounts;
+        }
+
+    }
+}
