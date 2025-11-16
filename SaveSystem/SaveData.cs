5ce90bafe72fd42a61d80132194d2d9a48a51769
RjanDoodles <rjsanchez023@gmail.com>
2025-11-16 12:47:22 -0600

    Thing ain't ignoring sorry for the spam
    Trying to use SaveData for personal reason lol

--- SaveSystem/SaveData.cs
+++ SaveSystem/SaveData.cs
@@ -4,11 +4,12 @@ using System.Text.Json;
 public class SaveData
 {
     //Put variables here, make sure your variables are public and with { get; set;} otherwise it won't save
+    public int highScore;
     /// <summary>
     /// Put the "New Game" variable values here
     /// </summary>
     public SaveData()
     {
-       
+       highScore = 0;
     }
 }
